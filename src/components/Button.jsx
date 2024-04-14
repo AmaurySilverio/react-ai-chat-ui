187f5695528d5101f522894e8e711b6f83c2035f
Maria Cristina Del Valle <32019417+mardelvalle@users.noreply.github.com>
2024-04-14 19:33:23 -0400

    add feedback styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,22 @@ import { Button } from '@mui/material'
 
 const CustomButton = ({ text, type, value, onClick }) => {
   return (
-    <Button sx={{backgroundColor: "red", "&:focus": {borderColor: "white", borderWidth: "0.1rem", borderStyle: "solid"}, "&:hover": {borderColor: "white", borderWidth: "0.1rem", borderStyle: "solid"}, color: "white", fontSize: "0.75rem", fontWeight: 700}} type={type} value={value} onClick={onClick}>
+    <Button sx={{
+      backgroundColor: "LightGray", 
+      color: "#212121",
+      "&:hover": {
+        borderColor: "purple", 
+        color: "white", 
+        borderWidth: "0.1rem", 
+        borderStyle: "solid"}, 
+        fontSize: "0.75rem", 
+        width: "inherit", 
+        fontWeight: 700
+      }} 
+      type={type} 
+      value={value} 
+      onClick={onClick}
+    >
       {text}
     </Button>
   );
